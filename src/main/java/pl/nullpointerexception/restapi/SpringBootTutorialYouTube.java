5151f699e14df3682770157b7bb8996ea93f314b
pilar <pilarusen.1@gmail.com>
2022-06-16 19:28:48 +0200

    initial commit

---
+++ src/main/java/pl/nullpointerexception/restapi/SpringBootTutorialYouTube.java
@@ -0,0 +1,15 @@
+package pl.nullpointerexception.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialYouTube {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialYouTube.class, args);
+	}
+
+}
+
+//swagger-ui.html
